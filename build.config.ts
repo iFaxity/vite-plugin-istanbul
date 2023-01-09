38fdd8147afa110ea506e2f31e0dd22375ae1294
iFaxity <christian@faxity.se>
2023-01-09 23:56:32 +0100

    build: Updated deps in build

--- build.config.ts
+++ build.config.ts
@@ -2,15 +2,7 @@ import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
   entries: [
-    'src/index'
-  ],
-  externals: [
-    'rollup',
-    'vite',
-    'istanbul-lib-instrument',
-    'test-exclude',
-    '@istanbuljs/load-nyc-config',
-    'picocolors'
+    'src/index',
   ],
   clean: true,
   declaration: true,
