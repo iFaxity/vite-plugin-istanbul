d0511900099c7f8fa13cbbc4d7e49e2f78e31c17
Tomohiko Ozawa <kota65535@gmail.com>
2023-06-19 04:51:48 +0900

    break: Fix source map (#113)
    * fix sourcemap line number
    
    * fix comment
    
    * use espree
    
    * fix function name

---
+++ src/source-map.ts
@@ -0,0 +1,19 @@
+import * as espree from 'espree'
+import {SourceMapGenerator} from 'source-map'
+
+
+export function createIdentitySourceMap(file: string, source: string) {
+    const gen = new SourceMapGenerator({ file });
+    const tokens = espree.tokenize(source, { loc: true, ecmaVersion: 'latest' });
+
+    tokens.forEach((token: any) => {
+        const loc = token.loc.start;
+        gen.addMapping({
+            source: file,
+            original: loc,
+            generated: loc
+        });
+    });
+
+    return JSON.parse(gen.toString())
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -51,3 +51,41 @@ declare module 'test-exclude' {
 
   export = TestExclude;
 }
+
+declare module 'espree' {
+  // https://github.com/eslint/espree#options
+  export interface Options {
+    comment?: boolean;
+    ecmaFeatures?: {
+      globalReturn?: boolean;
+      impliedStrict?: boolean;
+      jsx?: boolean;
+    };
+    ecmaVersion?:
+        | 3
+        | 5
+        | 6
+        | 7
+        | 8
+        | 9
+        | 10
+        | 11
+        | 12
+        | 2015
+        | 2016
+        | 2017
+        | 2018
+        | 2019
+        | 2020
+        | 2021
+        | 2022
+        | 2023
+        | 'latest';
+    loc?: boolean;
+    range?: boolean;
+    sourceType?: 'script' | 'module';
+    tokens?: boolean;
+  }
+  // https://github.com/eslint/espree#tokenize
+  export function tokenize(code: string, options?: Options): any;
+}
